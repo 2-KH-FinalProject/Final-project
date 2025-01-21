27ee41b85163d26f2176781c1263fa16befc1257
Jung-Dae_seung <eotmd1213@naver.com>
2025-01-21 16:38:02 +0900

    주소 재변경

--- spring-user/ET-Final-server/src/main/java/edu/kh/project/member/controller/MemberController.java
+++ spring-user/ET-Final-server/src/main/java/edu/kh/project/member/controller/MemberController.java
@@ -83,7 +83,7 @@ public class MemberController {
 	        ).getBytes()
 	    ), StandardCharsets.UTF_8);
 
-	    return "redirect:http://192.168.50.236:3000?" + state;
+	    return "redirect:http://final-project-react-individual.vercel.app/?" + state;
 	}
 
 	/**
